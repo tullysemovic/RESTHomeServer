2608447eed711dd78118b87478442cacdf3712d8
tullysemovic <35882137+tullysemovic@users.noreply.github.com>
2023-12-02 00:07:50 +1030

    Feature: Remove sensitive information from config

--- server/config.js
+++ server/config.js
@@ -1,19 +1,17 @@
 var config = {};
 
 config.plex = {
-  urlRefreshMovies: "http://10.0.0.10:32400/library/sections/1/refresh",
-  urlRefreshTv: "http://10.0.0.10:32400/library/sections/2/refresh",
-  token: "sxfDwnwBdVr2TWcbDwyR",
+  urlRefreshMovies: "plexMovieUrl",
+  urlRefreshTv: "plexTvUrl",
+  token: "plexToken",
 };
 
 config.overseerr = {
-  urlLogin: "http://streaming.home.local/api/v1/auth/local",
-  urlAvailabilitySync:
-    "http://streaming.home.local/api/v1/settings/jobs/availability-sync/run",
-  email: "ww9qhvhfng@privaterelay.appleid.com",
-  password: "Subes2005",
-  apiKey:
-    "MTY3NzExMzE0OTk2NzViZDZmODY5LTRkZjItNDM4MC05NzM2LTljZWYyN2Q3NThjZSk=",
+  urlLogin: "overseerrLoginUrl",
+  urlAvailabilitySync: "overseerrAvailabilitySyncUrl",
+  email: "overseerrEmail",
+  password: "overseerrPassowrd",
+  apiKey: "overseerrApiKey",
 };
 
 config.port = 3000;
